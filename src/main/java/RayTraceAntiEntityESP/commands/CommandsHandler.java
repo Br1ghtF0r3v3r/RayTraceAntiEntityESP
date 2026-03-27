4733ea83328f2c8b623ba50ae6d2062020a766e4
lycopod <96251071+lycopenie@users.noreply.github.com>
2026-03-28 01:09:29 +0700

    raycast PoC done

--- src/main/java/RayTraceAntiEntityESP/commands/CommandsHandler.java
+++ src/main/java/RayTraceAntiEntityESP/commands/CommandsHandler.java
@@ -1,10 +1,18 @@
 package RayTraceAntiEntityESP.commands;
 
+import RayTraceAntiEntityESP.engine.RaycastUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
 
 import static RayTraceAntiEntityESP.Main.plugin;
 import static RayTraceAntiEntityESP.misc.StringFormat.formatToString;
@@ -19,6 +27,16 @@ public class CommandsHandler implements CommandExecutor {
             if (args[0].equalsIgnoreCase("reload")) {
                 sender.sendMessage(formatToString(sender, "&aReloaded all configurations!"));
                 plugin.reloadConfigAll();
+            }
+
+            if (args[0].equalsIgnoreCase("nigger")) {
+                Collection<? extends Player> players = Bukkit.getOnlinePlayers();
+
+                sender.sendMessage(
+                        String.valueOf(
+                                RaycastUtils.isEntityVisible((Player) sender, (LivingEntity) ((Player) sender).getNearbyEntities(2d, 2d, 2d).getFirst())
+                        )
+                );
             }
             return true;
         }
