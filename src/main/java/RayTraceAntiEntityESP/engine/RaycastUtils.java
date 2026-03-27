de7c40ff33e81533735183632fac66363328913a
lycopod <luongcaophong1312@gmail.com>
2026-03-28 00:04:57 +0700

    refactor

---
+++ src/main/java/RayTraceAntiEntityESP/engine/RaycastUtils.java
@@ -0,0 +1,9 @@
+package RayTraceAntiEntityESP.engine;
+
+import org.bukkit.entity.Player;
+
+import java.util.Collection;
+
+public class RaycastUtils {
+    public static Collection<? extends Player> players;
+}
