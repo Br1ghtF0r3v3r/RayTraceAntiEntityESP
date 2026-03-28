c7b91b0f0040a014c4a05476f43c547587e3f3ec
lycopod <luongcaophong1312@gmail.com>
2026-03-28 21:45:40 +0700

    more features

---
+++ src/main/java/RayTraceAntiEntityESP/engine/EntityPacketHandler.java
@@ -0,0 +1,36 @@
+package RayTraceAntiEntityESP.engine;
+
+import com.github.retrooper.packetevents.PacketEvents;
+import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerDestroyEntities;
+import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerEntityMetadata;
+import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerSpawnEntity;
+import io.github.retrooper.packetevents.util.SpigotConversionUtil;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+
+public class EntityPacketHandler {
+    public void hideEntity(Player player, Entity target) {
+        int id = target.getEntityId();
+        WrapperPlayServerDestroyEntities destroyPacket = new WrapperPlayServerDestroyEntities(id);
+        PacketEvents.getAPI().getPlayerManager().sendPacket(player, destroyPacket);
+    }
+
+    private void spawnEntity(Player player, Entity target) {
+        WrapperPlayServerSpawnEntity spawn = new WrapperPlayServerSpawnEntity(
+                target.getEntityId(),
+                target.getUniqueId(),
+                SpigotConversionUtil.fromBukkitEntityType(target.getType()),
+                SpigotConversionUtil.fromBukkitLocation(target.getLocation()),
+                target.getLocation().getYaw(),
+                0, null
+        );
+
+        WrapperPlayServerEntityMetadata meta = new WrapperPlayServerEntityMetadata(
+                target.getEntityId(),
+                SpigotConversionUtil.getEntityMetadata(target)
+        );
+
+        PacketEvents.getAPI().getPlayerManager().sendPacket(player, spawn);
+        PacketEvents.getAPI().getPlayerManager().sendPacket(player, meta);
+    }
+}
