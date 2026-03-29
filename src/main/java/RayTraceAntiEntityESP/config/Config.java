fe35b131007fbbf74ebaf015bfa33cd8a7352bb9
brightforever <vanhtrau09@gmail.com>
2026-03-29 11:49:38 +0700

    random fixes

--- src/main/java/RayTraceAntiEntityESP/config/Config.java
+++ src/main/java/RayTraceAntiEntityESP/config/Config.java
@@ -15,7 +15,7 @@ public class Config {
     public static void setConfig() {
 
         boundingBoxExtraValue = plugin.getConfig().getDouble("bounding_box_extra_value", 0.1);
-        samplePointsPerCorner = plugin.getConfig().getInt("sample_points_per_corner", 8);
+        samplePointsPerCorner = plugin.getConfig().getInt("sample_points", 8);
         distanceOverride = plugin.getConfig().getDouble("distances_override", -1);
         isPerspectiveCheckingEnabled = plugin.getConfig().getBoolean("perspective_checking.enabled", true);
         perspectiveCheckingDistance = plugin.getConfig().getDouble("perspective_checking.distances", 4);
