6b5c8451bbdcda9f0b0eb66b3e5a5523a6b68956
brightforever <vanhtrau09@gmail.com>
2026-03-29 11:30:03 +0700

    add enabled in commands

--- src/main/java/RayTraceAntiEntityESP/config/Config.java
+++ src/main/java/RayTraceAntiEntityESP/config/Config.java
@@ -7,18 +7,20 @@ public class Config {
     public static double boundingBoxExtraValue;
     public static int samplePointsPerCorner;
     public static double distanceOverride;
-    public static boolean perspectiveCheckingEnabled;
+    public static boolean isPerspectiveCheckingEnabled;
     public static double perspectiveCheckingDistance;
     public static long checkingIntervalTicks;
+    public static boolean isCheckingEnabled;
 
     public static void setConfig() {
 
         boundingBoxExtraValue = plugin.getConfig().getDouble("bounding_box_extra_value", 0.1);
         samplePointsPerCorner = plugin.getConfig().getInt("sample_points_per_corner", 8);
         distanceOverride = plugin.getConfig().getDouble("distances_override", -1);
-        perspectiveCheckingEnabled = plugin.getConfig().getBoolean("perspective_checking.enabled", true);
+        isPerspectiveCheckingEnabled = plugin.getConfig().getBoolean("perspective_checking.enabled", true);
         perspectiveCheckingDistance = plugin.getConfig().getDouble("perspective_checking.distances", 4);
         checkingIntervalTicks = plugin.getConfig().getLong("checking_interval_ticks", 1);
+        isCheckingEnabled = plugin.getConfig().getBoolean("enabled", true);
 
     }
 
