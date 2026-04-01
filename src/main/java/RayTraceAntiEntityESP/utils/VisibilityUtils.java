3ff747b1ba70edb625a2ffcbf8ec54efcba0339e
brightforever <vanhtrau09@gmail.com>
2026-04-01 19:04:00 +0700

    unified ray trace task to debug and fake name task - remove and add unnecessary and necessary conditions

--- src/main/java/RayTraceAntiEntityESP/utils/VisibilityUtils.java
+++ src/main/java/RayTraceAntiEntityESP/utils/VisibilityUtils.java
@@ -1,10 +1,14 @@
 package RayTraceAntiEntityESP.utils;
 
+import RayTraceAntiEntityESP.config.Config;
 import RayTraceAntiEntityESP.manager.engine.PacketFilterManager;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.Team;
 
 import static RayTraceAntiEntityESP.Main.plugin;
+import static RayTraceAntiEntityESP.utils.TeamUtils.getTeam;
+import static RayTraceAntiEntityESP.utils.TeamUtils.getTeamVisibility;
 
 public class VisibilityUtils {
 
@@ -12,13 +16,39 @@ public class VisibilityUtils {
         PacketFilterManager.bypassPacketSet.add(PacketFilterManager.bypassHiddenKey(player, entity.getUniqueId()));
         player.hideEntity(plugin, entity);
 
-        FakeNameDisplay.applyDisplay(player, entity);
+        if (Config.isFakeDisplayNameEnabled) {
+            FakeNameDisplay.applyDisplay(player, entity);
+        }
     }
 
     public static void setNotHidden(Player player, Entity entity) {
+        PacketFilterManager.bypassPacketSet.remove(PacketFilterManager.bypassHiddenKey(player, entity.getUniqueId()));
+        player.hideEntity(plugin, entity);
+        
         PacketFilterManager.bypassPacketSet.add(PacketFilterManager.bypassShowKey(player, entity.getUniqueId()));
         player.showEntity(plugin, entity);
 
-        FakeNameDisplay.removeDisplay(player, entity);
+        if (Config.isFakeDisplayNameEnabled) {
+            FakeNameDisplay.removeDisplay(player, entity);
+        }
+    }
+
+    public static boolean isNameVisible(Player viewer, Entity entity) {
+        if (entity.isInvisible()) return false;
+
+        if (!(entity instanceof Player)) {
+            if (entity.customName() == null || !entity.isCustomNameVisible()) return false;
+        }
+
+        Team viewerTeam = getTeam(viewer);
+        Team entityTeam = getTeam(entity);
+        boolean onSameTeam = viewerTeam != null && viewerTeam.equals(entityTeam);
+
+        return switch (getTeamVisibility(entityTeam)) {
+            case ALWAYS -> true;
+            case NEVER -> false;
+            case FOR_OWN_TEAM -> onSameTeam;
+            case FOR_OTHER_TEAMS -> !onSameTeam;
+        };
     }
 }
