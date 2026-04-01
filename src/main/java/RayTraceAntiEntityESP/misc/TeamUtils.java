260ad388f6fd4c3ea441674e071e7d50a05aac27
lycopod <luongcaophong1312@gmail.com>
2026-04-01 13:40:20 +0700

    refactor: clean up imports

--- src/main/java/RayTraceAntiEntityESP/misc/TeamUtils.java
+++ src/main/java/RayTraceAntiEntityESP/misc/TeamUtils.java
@@ -2,15 +2,16 @@ package RayTraceAntiEntityESP.misc;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
+import org.bukkit.scoreboard.Team;
 
 public class TeamUtils {
-    public static org.bukkit.scoreboard.Team getTeam(Entity entity) {
+    public static Team getTeam(Entity entity) {
         return Bukkit.getScoreboardManager().getMainScoreboard().getEntryTeam(entity.getScoreboardEntryName());
     }
 
-    public static org.bukkit.scoreboard.Team.OptionStatus getTeamVisibility(org.bukkit.scoreboard.Team team) {
-        if (team == null) return org.bukkit.scoreboard.Team.OptionStatus.ALWAYS;
-        return team.getOption(org.bukkit.scoreboard.Team.Option.NAME_TAG_VISIBILITY);
+    public static Team.OptionStatus getTeamVisibility(Team team) {
+        if (team == null) return Team.OptionStatus.ALWAYS;
+        return team.getOption(Team.Option.NAME_TAG_VISIBILITY);
     }
 
 }
