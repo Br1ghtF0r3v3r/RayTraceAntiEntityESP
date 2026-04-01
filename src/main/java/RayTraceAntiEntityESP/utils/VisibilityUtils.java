5a5ec25c2b36d88e232845ab8fee7744e4143384
brightforever <vanhtrau09@gmail.com>
2026-04-01 17:00:14 +0700

    change condition for PLAYER_INFO_REMOVE and rename all methods in DebugsUtils.java

--- src/main/java/RayTraceAntiEntityESP/utils/VisibilityUtils.java
+++ src/main/java/RayTraceAntiEntityESP/utils/VisibilityUtils.java
@@ -1,23 +1,24 @@
 package RayTraceAntiEntityESP.utils;
 
+import RayTraceAntiEntityESP.manager.engine.PacketFilterManager;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 
 import static RayTraceAntiEntityESP.Main.plugin;
-import static RayTraceAntiEntityESP.manager.engine.PacketFilterManager.addPacketBypass;
 
 public class VisibilityUtils {
 
-    public static void setHidden(Player viewer, Entity entity) {
-        viewer.hideEntity(plugin, entity);
-        FakeNameDisplay.applyDisplay(viewer, entity);
+    public static void setHidden(Player player, Entity entity) {
+        PacketFilterManager.bypassPacketSet.add(PacketFilterManager.bypassHiddenKey(player, entity.getUniqueId()));
+        player.hideEntity(plugin, entity);
+
+        FakeNameDisplay.applyDisplay(player, entity);
     }
 
-    public static void setNotHidden(Player viewer, Entity entity) {
-        viewer.hideEntity(plugin, entity);
-        addPacketBypass(viewer, entity.getUniqueId());
-        viewer.showEntity(plugin, entity);
+    public static void setNotHidden(Player player, Entity entity) {
+        PacketFilterManager.bypassPacketSet.add(PacketFilterManager.bypassShowKey(player, entity.getUniqueId()));
+        player.showEntity(plugin, entity);
 
-        FakeNameDisplay.removeDisplay(viewer, entity);
+        FakeNameDisplay.removeDisplay(player, entity);
     }
 }
