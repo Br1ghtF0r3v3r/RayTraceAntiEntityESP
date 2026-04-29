f6e64202425ed3f8539eeb5e72584654886744f5
brightforever <vanhtrau09@gmail.com>
2026-04-29 20:42:42 +0700

    optimize by snapshot, async run and everything possible

--- src/main/java/RayTraceAntiEntityESP/bukkit/manager/events/EventManager.java
+++ src/main/java/RayTraceAntiEntityESP/bukkit/manager/events/EventManager.java
@@ -1,6 +1,5 @@
 package RayTraceAntiEntityESP.bukkit.manager.events;
 
-import RayTraceAntiEntityESP.bukkit.config.Config;
 import RayTraceAntiEntityESP.bukkit.manager.engine.PacketManager;
 import RayTraceAntiEntityESP.bukkit.manager.engine.NametagCloneManager;
 import RayTraceAntiEntityESP.bukkit.manager.engine.VerticesDebugManager;
@@ -9,7 +8,6 @@ import com.github.retrooper.packetevents.event.PacketSendEvent;
 import io.papermc.paper.event.player.*;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.Player;
 import org.bukkit.event.block.*;
 import org.bukkit.event.entity.*;
 import org.bukkit.event.inventory.*;
@@ -18,92 +16,27 @@ import org.bukkit.event.player.*;
 import java.util.UUID;
 
 import static RayTraceAntiEntityESP.bukkit.Main.plugin;
+import static RayTraceAntiEntityESP.bukkit.config.Config.isDebugEnabled;
 import static RayTraceAntiEntityESP.bukkit.config.Config.isDisplayNameEnabled;
 
 public class EventManager {
 
-    public static void regainHealthManager(EntityRegainHealthEvent event) {
-    }
-
-    public static void statisticIncementManager(PlayerStatisticIncrementEvent event) {
-    }
-
-    public static void playerDeathManager(PlayerDeathEvent event) {
-    }
-
-    public static void entityDamageManager(EntityDamageByEntityEvent event) {
-    }
-
-    public static void playerInteractManager(PlayerInteractEvent event) {
-    }
-
-    public static void playerMoveManager(PlayerMoveEvent event) {
-    }
-
-    public static void playerTeleportManager(PlayerMoveEvent event) {
-    }
-
-    public static void inventoryClickManager(InventoryClickEvent event) {
-    }
-
-    public static void itemSpawnManager(ItemSpawnEvent event) {
-    }
-
-    public static void prepareCraftManager(PrepareItemCraftEvent event) {
-    }
-
-    public static void inventorySlotChangeManager(PlayerInventorySlotChangeEvent event) {
-    }
-
-    public static void dropManager(PlayerDropItemEvent event) {
-    }
-
-    public static void playerSwapHandItemsManager(PlayerSwapHandItemsEvent event) {
-    }
-
-    public static void playerPickupExperienceManager(PlayerExpChangeEvent event) {
-    }
-
-    public static void blockExplodeManager(BlockExplodeEvent event) {
-    }
-
-    public static void prepareSmithingManager(PrepareSmithingEvent event) {
-    }
-
-    public static void prepareGrindstoneManager(PrepareGrindstoneEvent event) {
-    }
-
-    public static void blockPlaceManager(BlockPlaceEvent event) {
-    }
-
-    public static void blockBreakManager(BlockBreakEvent event) {
-    }
-
-    public static void playerPickupArrowManager(PlayerPickupArrowEvent event) {
-    }
-
-    public static void playerJoinManager(PlayerJoinEvent event) {
-    }
-
     public static void connectionCloseManager(PlayerConnectionCloseEvent event) {
         UUID playerUUID = event.getPlayerUniqueId();
-
-        if (isDisplayNameEnabled) {
-            Bukkit.getScheduler().runTaskLater(plugin, () -> {
+        Bukkit.getScheduler().runTaskLater(plugin, () -> {
+            if (isDisplayNameEnabled) {
                 NametagCloneManager.removeDisplay(playerUUID);
-                VerticesDebugManager.removeDisplay(playerUUID);
                 NametagCloneManager.removeDisplayForEntity(playerUUID);
+            }
+            if (isDebugEnabled) {
+                VerticesDebugManager.removeDisplay(playerUUID);
                 VerticesDebugManager.removeDisplayForEntity(playerUUID);
-            }, 20L);
-        }
-    }
-
-    public static void playerLeaveManager(PlayerQuitEvent event) {
+            }
+        }, 20L);
     }
 
     public static void packetSendManager(PacketSendEvent event) {
         PacketManager.packetManager(event);
-
     }
 
     public static void entityDeathManager(EntityDeathEvent event) {
@@ -112,6 +45,8 @@ public class EventManager {
 
         if (isDisplayNameEnabled) {
             NametagCloneManager.removeDisplayForEntity(entityUUID);
+        }
+        if (isDebugEnabled) {
             VerticesDebugManager.removeDisplayForEntity(entityUUID);
         }
     }
