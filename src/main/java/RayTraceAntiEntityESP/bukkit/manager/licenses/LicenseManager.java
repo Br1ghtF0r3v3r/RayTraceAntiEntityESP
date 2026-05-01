b6c1a6df07659a188873d5c8f4f4452531d3ed3e
brightforever <vanhtrau09@gmail.com>
2026-05-01 21:28:40 +0700

    remove build id because its dumb

--- src/main/java/RayTraceAntiEntityESP/bukkit/manager/licenses/LicenseManager.java
+++ src/main/java/RayTraceAntiEntityESP/bukkit/manager/licenses/LicenseManager.java
@@ -3,30 +3,9 @@ package RayTraceAntiEntityESP.bukkit.manager.licenses;
 import dev.respark.licensegate.LicenseGate;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
 public class LicenseManager {
 
     private static final String USER_ID = "a267a";
-    private static String buildId = null;
-
-    public static String getBuildId(JavaPlugin plugin) {
-        if (buildId != null) return buildId;
-        try (InputStream is = plugin.getClass().getClassLoader().getResourceAsStream("build.properties")) {
-            if (is == null) {
-                plugin.getLogger().severe("build.properties not found!");
-                return null;
-            }
-            Properties props = new Properties();
-            props.load(is);
-            buildId = props.getProperty("build.id");
-        } catch (IOException e) {
-            plugin.getLogger().severe("Failed to read build.properties: " + e.getMessage());
-        }
-        return buildId;
-    }
 
     public static boolean verifyLicense(String licenseKey, JavaPlugin plugin) {
         boolean licensed = false;
@@ -36,16 +15,9 @@ public class LicenseManager {
             return false;
         }
 
-        String id = getBuildId(plugin);
-        if (id == null) {
-            plugin.getLogger().severe("Could not determine build ID, license check aborted!");
-            return false;
-        } else {
-            plugin.getLogger().info("Your build ID: " + id);
-        }
         try {
             LicenseGate licenseGate = new LicenseGate(USER_ID);
-            LicenseGate.ValidationType result = licenseGate.verify(licenseKey, id);
+            LicenseGate.ValidationType result = licenseGate.verify(licenseKey);
             if (result == LicenseGate.ValidationType.VALID) {
                 licensed = true;
                 plugin.getLogger().info("License verified!");
