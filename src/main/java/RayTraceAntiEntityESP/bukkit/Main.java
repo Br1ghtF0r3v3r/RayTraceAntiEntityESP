9fe25b1f1d39a79e6852ec9d17000c4960ed2251
brightforever <vanhtrau09@gmail.com>
2026-06-07 05:41:32 +0700

    test webhook

--- src/main/java/RayTraceAntiEntityESP/bukkit/Main.java
+++ src/main/java/RayTraceAntiEntityESP/bukkit/Main.java
@@ -12,7 +12,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class Main extends JavaPlugin {
 
     public static Main plugin;
-
     @Override
     public void onEnable() {
         plugin = this;
