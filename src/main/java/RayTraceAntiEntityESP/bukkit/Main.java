4e97b5d021ec62ddde81dbf34f2bb50e414a2bf9
brightforever <vanhtrau09@gmail.com>
2026-06-07 05:45:57 +0700

    Revert "test webhook"
    This reverts commit 1d55cd161dc6d0de597e8dd3949164bdb29f5a73.

--- src/main/java/RayTraceAntiEntityESP/bukkit/Main.java
+++ src/main/java/RayTraceAntiEntityESP/bukkit/Main.java
@@ -12,6 +12,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class Main extends JavaPlugin {
 
     public static Main plugin;
+
     @Override
     public void onEnable() {
         plugin = this;
